d05fba8dd7ab8f13a100fc0c26150a75d4eb790c
xilko.2020 <xilko.2020@CLOUD-RDHOST1>
2023-04-19 13:34:00 +0700

    Добавьте файлы проекта.

---
+++ State/State.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <string>
+using namespace std;
+class State {
+public:
+    virtual ~State() {}
+    virtual void insertQuarter() = 0;
+    virtual void ejectQuarter() = 0;
+    virtual void turnCrank() = 0;
+    virtual void dispense() = 0;
+    virtual string toString() = 0;
+};
